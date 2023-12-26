3c38ca500d425cb9edc1d8d6b232bc1d5a375bf3
Seu Nome <seu-email@example.com>
2023-12-26 20:18:46 -0300

    codigo inicial

---
+++ CodigoTeste.py
@@ -0,0 +1,10 @@
+arquivo = open('cadastros.csv', 'a')
+
+nome        = input('Digite o nome: ')
+cadastro    = input('Digite o cadastro: ')
+funcao      = input('Digite o funcao: ')
+
+arquivo.write(nome+';'+cadastro+';'+funcao+'\n')
+arquivo.close()
+
+
